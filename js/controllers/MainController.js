2545ba0fed9663b8b64ba63eae92c0a4463fa5d3
Eivind <emelleby@gmail.com>
2015-12-17 13:47:13 +0700

    Add checkAll function to checkboxes and add an array of results from ng-repeat.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,8 @@
+'use strict';
+// Controller
+
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+	
   $scope.title = 'Fixed + Fluid Bootstrap Template with Off-canvas Sidebar';
 	
 	$http.get('../data/test.json').then(function (response) {
@@ -11,8 +15,17 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	  	};
 
 	  $scope.checkAll = function() {
-		$scope.categories.roles = angular.copy($scope.roles);
+		  angular.forEach($scope.results, function(value, key) {
+			  this.push($scope.results[key].category);
+			}, $scope.categories.roles);
+		  
+		//$scope.categories.roles = angular.copy($scope.categories.roles);
+		//$scope.categories.roles.push('Arts');
 	  };
+	
+	
+	
+	
 
 	  $scope.uncheckAll = function() {
 		$scope.categories.roles = [];
@@ -26,6 +39,42 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	 $scope.log = function() {
 		console.log($scope.categories.roles);
 		 console.log($scope.data);
+		 console.log($scope.results[0].category);
 	  };
+	
+	// Tabs from activitycontroller
+	
+                  $scope.tab = 1;
+
+                  $scope.filtText = 'monday';
+
+                  $scope.select = function(setTab) {
+                    $scope.tab = setTab;
+                    
+                    if (setTab === 2) {
+                      $scope.filtText = "tuesday";
+                    }
+                    else if (setTab === 3) {
+                      $scope.filtText = "wednesday";
+                    }
+                    else if (setTab === 4) {
+                      $scope.filtText = "thursday";
+                    }
+					  else if (setTab === 5) {
+                      $scope.filtText = "friday";
+                    }
+                    else {
+                      $scope.filtText = "monday";
+                    }
+                  };
+
+                  $scope.isSelected = function (checkTab) {
+                    return ($scope.tab === checkTab);
+                  };
+
+                  $scope.showDetails = false;
+                  $scope.toggleDetails = function() {
+                    $scope.showDetails = !$scope.showDetails;
+                };
   
 }]);
