ea60f9e5e69a93504152df247a1726cd7488e711
Eivind <emelleby@gmail.com>
2015-12-17 17:43:57 +0700

    Skal nå endre objektet til en array og bytte ut roles.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,32 +1,40 @@
 'use strict';
 // Controller
 
-app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+app.controller('MainController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 	
   $scope.title = 'Fixed + Fluid Bootstrap Template with Off-canvas Sidebar';
 	
 	$http.get('../data/test.json').then(function (response) {
 			$scope.data = response.data;
-			console.log($scope.data);
+			//console.log($scope.data);
 		})
+		$scope.filText2 = [];
 	
 		$scope.categories = {
 			roles: []
 	  	};
+	
+	$scope.$watch(function () {
+		// $scope.filText2 = angular.copy($scope.categories.roles);	
+		// code here
+		// if (category in in categories array)
+		//	show item
+		
+		//else
+			// hide item
+			});
 
-	  $scope.checkAll = function() {
+	 $scope.checkAll = function() {
+		  $scope.categories.roles = [];
 		  angular.forEach($scope.results, function(value, key) {
 			  this.push($scope.results[key].category);
 			}, $scope.categories.roles);
 		  
-		//$scope.categories.roles = angular.copy($scope.categories.roles);
+		// $scope.kategori = angular.copy($scope.categories.roles);
 		//$scope.categories.roles.push('Arts');
 	  };
 	
-	
-	
-	
-
 	  $scope.uncheckAll = function() {
 		$scope.categories.roles = [];
 	  };
@@ -39,7 +47,9 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	 $scope.log = function() {
 		console.log($scope.categories.roles);
 		 console.log($scope.data);
-		 console.log($scope.results[0].category);
+	angular.copy($scope.categories.roles, $scope.filText2);
+		 console.log($scope.results);
+		 console.log($scope.filtText2);
 	  };
 	
 	// Tabs from activitycontroller
@@ -74,7 +84,21 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 
                   $scope.showDetails = false;
                   $scope.toggleDetails = function() {
-                    $scope.showDetails = !$scope.showDetails;
+                  $scope.showDetails = !$scope.showDetails;
                 };
+	
+	
+		$scope.$watch(function () {
+			$scope.results = $scope.$eval("data.ecas | unique: 'category'");
+			});
+	
+		var promise = $timeout(function () {
+			angular.forEach($scope.results, function(value, key) {
+				  this.push($scope.results[key].category);
+					}, $scope.categories.roles);
+			
+		}, 1000);
+			  
+		
   
 }]);
