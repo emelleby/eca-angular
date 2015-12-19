668498099b54922f05e4402206b3c60aac878a90
Eivind <emelleby@gmail.com>
2015-12-19 12:57:11 +0700

    Add material tabs to ecaPanel3

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -65,7 +65,28 @@ app.controller('MainController', ['$scope', '$http', '$timeout', function($scope
 		 console.log($scope.cost);
 	  };
 	
-	// Tabs from activitycontroller
+	/* Tabs from material design ecaPanel2
+	selected = null,
+    previous = null;
+	
+    $scope.tabs = tabs;
+    $scope.selectedIndex = 2;
+    $scope.$watch('selectedIndex', function(current, old){
+      previous = selected;
+      selected = tabs[current];
+      if ( old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
+      if ( current + 1 )                $log.debug('Hello ' + selected.title + '!');
+    });
+    $scope.addTab = function (title, view) {
+      view = view || title + " Content View";
+      tabs.push({ title: title, content: view, disabled: false});
+    };
+    $scope.removeTab = function (tab) {
+      var index = tabs.indexOf(tab);
+      tabs.splice(index, 1);
+    };
+	*/
+	// Tabs from Bootstrap
 	
                   $scope.tab = 1;
 
@@ -94,10 +115,16 @@ app.controller('MainController', ['$scope', '$http', '$timeout', function($scope
                   $scope.isSelected = function (checkTab) {
                     return ($scope.tab === checkTab);
                   };
+	
+	              $scope.toggleModel = function(id) {
+                  	$scope.toggleModel.id = !$scope.toggleModel.id;
+					  console.log($scope.toggleModel.id);
+                };
 
                   $scope.showDetails = false;
                   $scope.toggleDetails = function() {
                   $scope.showDetails = !$scope.showDetails;
+					  console.log($scope.showDetails);
                 };
 	
 		
